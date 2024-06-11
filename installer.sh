69a2efaa5da5d43968a8636815b894c1aa68088b
kramankishore <k.ramankishore@gmail.com>
2024-06-11 09:07:46 +0530

    Update installer.sh - minor change

--- installer.sh
+++ installer.sh
@@ -91,7 +91,7 @@ sudo apt install -y picom rofi dunst libnotify-bin unzip wmctrl xdotool libnotif
 
 # Others
 # sudo apt install -y numlockx figlet galculator cpu-x udns-utils whois curl tree
-sudo apt install -y gcalculator whois curl tree
+sudo apt install -y galculator whois curl tree
 
 # Screenshots - Flameshot
 sudo apt install flameshot
